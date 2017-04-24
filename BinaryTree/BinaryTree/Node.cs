a45555d10a7a057da24107650dcbc9493708b2b5
Evan-Farr <evan.c.farr@gmail.com>
2017-04-24 09:47:41 -0500

    created github file

---
+++ BinaryTree/BinaryTree/Node.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class Node<T> where T : IComparable
+    {
+        private T nodeValue;
+        private Node<T> parent;
+        private Node<T> children;
+        private Node<T> nodeLeft;
+        private Node<T> nodeRight;
+
+        public T NodeValue { get { return nodeValue; } set { nodeValue = value; } }
+        public Node<T> Parent { get { return parent; } }
+        public Node<T> Children { get { return children; } set { children = value; } }
+        public Node<T> NodeLeft { get { return nodeLeft; } set { nodeLeft = value; } }
+        public Node<T> NodeRight { get { return nodeRight; } set { nodeRight = value; } }
+
+        public Node(T nodeValue)
+        {
+            this.nodeValue = nodeValue;
+        }
+
+        public override string ToString()
+        {
+            return nodeValue.ToString();
+        }
+    }
+}
---
+++ BinaryTree/BinaryTree/Tree.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class Tree<T> where T : IComparable
+    {
+        private int count;
+        private Node<T> root;
+
+        public int Count { get { return count; } set { count = value; } }
+        public Node<T> Root { get { return root; } set { root = value; } }
+
+        public Tree(Node<T> root)
+        {
+            this.root = root;
+            count = 1;
+        }
+
+        public void Add(Node<T> node, T value)
+        {
+            if ()
+            {
+            }
+        }
+
+        public void Search(T value)
+        {
+            var startPoint = root;
+        }
+    }
+}
